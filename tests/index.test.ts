080bb37b5bf0fb5b760cba500b39f7ebba839af6
Elry <rafaelgarcialima@outlook.com>
2020-10-22 20:12:51 -0300

    Fixed testing on linux

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,3 +1,4 @@
+import 'mocha';
 import { Main } from '../src/index';
 import { expect, assert } from 'chai';
 
@@ -9,25 +10,4 @@ describe('Main', () => {
       expect(main.looper()).to.be.an('array');
     });
   });
-
-  // describe('#setFive()', () => {
-  //   it('Should return five with [0] as upper case', () => {
-  //     expect(main.setFive(5)).to.be.equal('Five');
-  //   });
-  //   it('Should not return five in any way', () => {
-  //     expect(main.setFive(4)).to.be.null;
-  //   });
-  // });
-
-  // describe('#setThree()', () => {
-  //   it('Should return Three', () => {
-  //     assert.equal(main.setThree(3), 'Three');
-  //   });
-  // });
-
-  // describe('#setThreeFive()', () => {
-  //   it('Should return ThreeAndFive', () => {
-  //     assert.equal(main.setFT(15), 'ThreeAndFive');
-  //   });
-  // });
 });
