c494d85e8107f430fece27f2e4a9c9e2b0e1e499
Rafael_Garcia <pmsp.smti.dev@hotmail.com>
2020-10-23 15:01:38 -0300

    test partially refactored

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,19 +1,51 @@
 import 'mocha';
-import { Main } from '../src/index';
-import { expect, assert } from 'chai';
+import { expect } from 'chai';
+import { Showing } from '../src/index';
 
 describe('Main', () => {
-  const main = new Main();
+  const showing = new Showing();
   
-  describe('#looper()', () => {
-    it('Should return array', () => {
-      expect(main.looper()).to.be.an('array');
-    });
-  });
+  describe('Mapping', () => {
+    describe('Result', () => {
+      it('Should be instance of Map object', () => {
+        expect(showing.getResult()).instanceOf(Map);
+      });
+  
+      it('Map object should not be empty', () => {
+        expect(showing.getResult()).to.not.be.empty;
+      });
+  
+      it('Have size equal to 100', () => {
+        expect(showing.getResult().size).to.be.equal(100);
+      });
 
-  describe('#mapper()', () => {
-    it('Should return mapper', () => {
-      expect(main.mapper()).to.be.an('array');
+      it('Should have all strings (Five, Three, ThreeAndFive) required', () => {
+        let mapValues =  Array.from(showing.getResult().values());
+  
+        expect(mapValues.includes('Five')).to.be.true;
+        expect(mapValues.includes('Three')).to.be.true;
+        expect(mapValues.includes('ThreeAndFive')).to.be.true;
+      });
+    });
+
+    describe('GeneralMapping', () => {
+      it('Should be instance of Map object', () => {
+        expect(showing.getFiveMap()).instanceOf(Map);
+        expect(showing.getThreeMap()).instanceOf(Map);
+        expect(showing.getNormalMap()).instanceOf(Map);
+      });
+  
+      it('Map object should not be empty', () => {
+        expect(showing.getFiveMap()).to.not.be.empty;
+        expect(showing.getThreeMap()).to.not.be.empty;
+        expect(showing.getNormalMap()).to.not.be.empty;
+      });
+  
+      it('Have expected size', () => {
+        expect(showing.getNormalMap().size).to.be.equal(100);
+        expect(showing.getFiveMap().size).to.be.equal(Math.round(100 / 5));
+        expect(showing.getThreeMap().size).to.be.equal(Math.round(100 / 3));
+      });
     });
   });
 });
