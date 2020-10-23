2d87e04cb253d9afc0f876dbadf0de548c26136a
Elry <rafaelgarcialima@outlook.com>
2020-10-22 21:58:31 -0300

    newer function and conditions reduced

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,16 @@ export class Main{
     for(let a = 1;a <= 100; a++){
       simpleArr.push(this.checker(a));
     }
-    console.log(simpleArr);
+    
     return simpleArr;
   }
 
   setThree = (val:number) => { return (val % 3 === 0) ? "Three" : val; }
   setFive = (val:number) => { return (val % 5 === 0) ? "Five" : this.setThree(val); }
   checker = (val:number) => { return (val % 3 === 0 && val % 5 === 0) ? "ThreeAndFive" : this.setFive(val); }
+
+  mapper = () => {
+    let tMap = [], fMap = {}, rMap = {}, a = 0;
+    return [];
+  }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -10,4 +10,10 @@ describe('Main', () => {
       expect(main.looper()).to.be.an('array');
     });
   });
+
+  describe('#mapper()', () => {
+    it('Should return mapper', () => {
+      expect(main.mapper()).to.be.an('array');
+    });
+  });
 });
